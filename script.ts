14abe2715a27287972689cedfad4e698939a4f8a
theophoric <theophoric@users.noreply.github.com>
2021-09-15 13:11:17 -0700

    minor schema tweak; script testing

--- script.ts
+++ script.ts
@@ -4,10 +4,10 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  const acCount = await prisma.account.count();
-
-
-  console.dir(acCount, { depth: null })
+  const aurora = await prisma.receipt.findMany({
+    take: 5
+  })
+  console.dir(aurora)
 }
 
 main()
